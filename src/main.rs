7f47e417c345ad72d8a7c32f22206e2bf0548560
MichielN19 <Michielmitsjol@gmail.com>
2025-11-07 13:58:53 +0100

    feat: most used memory resources

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,16 @@ fn main() {
         println!("\n=== Memory ===");
         println!("Total: {} MB", monitor.total_memory() / 1024 / 1024);
         println!("Used: {} MB", monitor.used_memory() / 1024 / 1024);
+        println!("Availible: {} MB", monitor.availible_memory() / 1024 / 1024);
+
+        println!("\n=== Top Processes (by Memory) ===");
+        let mut processes = monitor.processes();
+
+        processes.sort_by(|a, b| b.2.cmp(&a.2));
+
+        for (i, (name, cpu, memory)) in processes.iter().take(15).enumerate() {
+            println!("{:?}", name);
+        }
 
         thread::sleep(Duration::from_secs(1));
     }
--- src/monitor.rs
+++ src/monitor.rs
@@ -1,4 +1,4 @@
-use sysinfo::{Component, Disks, Networks, System};
+use sysinfo::{Component, Disks, Networks, System, Process};
 
 pub struct Monitor {
     sys: System,
@@ -11,12 +11,29 @@ impl Monitor {
         }
     }
 
+    pub fn processes(&self) -> Vec<(Option<&str>, f32, u64)> {
+        self.sys.processes()
+            .values()
+            .map(|process| {
+                (
+                    process.name().to_str(),
+                    process.cpu_usage(),
+                    process.memory(),
+                )
+            })
+            .collect()
+    }
+
     pub fn total_memory(&self) -> u64 {
         self.sys.total_memory()
     }
 
     pub fn used_memory(&self) -> u64 {
         self.sys.used_memory()
+    }
+    
+    pub fn availible_memory(&self) -> u64 {
+        self.sys.available_memory()
     }
 
     pub fn cpu_usage(&self) -> Vec<f32> {
